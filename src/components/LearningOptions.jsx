493a8df5569bacafcaee5326abf01238e94846d7
Matt-Chisholm <mathesonchisholm@hotmail.com>
2022-10-24 10:42:16 -0700

    widgets

--- src/components/LearningOptions.jsx
+++ src/components/LearningOptions.jsx
@@ -25,19 +25,3 @@ const LearningOptions = (props) => {
 };
 
 export default LearningOptions;
-
-// in src/components/LearningOptions/LearningOptions.css
-
-.learning-options-container {
-  display: flex;
-  align-items: flex-start;
-  flex-wrap: wrap;
-}
-
-.learning-option-button {
-  padding: 0.5rem;
-  border-radius: 25px;
-  background: transparent;
-  border: 1px solid green;
-  margin: 3px;
-}
--- src/config.js
+++ src/config.js
@@ -1,9 +1,18 @@
 import { createChatBotMessage } from "react-chatbot-kit";
+import LearningOptions from "./components/LearningOptions";
 
 const config = {
   botName: "Mattbot",
   initialMessages: [
-    createChatBotMessage(`Hi, I'm here to help. What can I do for you?`),
+    createChatBotMessage(`Hi, I'm here to help. What can I do for you?`, {
+      widget: "learningOptions",
+    }),
+  ],
+  widgets: [
+    {
+      widgetName: "learningOptions",
+      widgetFunc: (props) => <LearningOptions {...props} />,
+    },
   ],
   customStyles: {
     botMessageBox: {
