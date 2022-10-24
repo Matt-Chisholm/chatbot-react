d3051f750a5a5f8b3231f4dce65bbaf6895ff614
Matt-Chisholm <mathesonchisholm@hotmail.com>
2022-10-24 10:48:51 -0700

    resources for widgets

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 import { createChatBotMessage } from "react-chatbot-kit";
 import LearningOptions from "./components/LearningOptions";
+import LinkList from "./components/LinkList";
 
 const config = {
   botName: "Mattbot",
@@ -12,6 +13,29 @@ const config = {
     {
       widgetName: "learningOptions",
       widgetFunc: (props) => <LearningOptions {...props} />,
+    },
+    {
+      widgetName: "javascriptLinks",
+      widgetFunc: (props) => <LinkList {...props} />,
+      props: {
+        options: [
+          {
+            text: "Introduction to JavaScript",
+            url: "https://developer.mozilla.org/en-US/docs/Web/JavaScript/A_re-introduction_to_JavaScript",
+            id: 1,
+          },
+          {
+            text: "Mozilla JS Guide",
+            url: "https://developer.mozilla.org/en-US/docs/Web/Javascript/Guide",
+            id: 2,
+          },
+          {
+            text: "Frontend Masters",
+            url: "https://frontendmasters.com",
+            id: 3,
+          },
+        ],
+      },
     },
   ],
   customStyles: {
