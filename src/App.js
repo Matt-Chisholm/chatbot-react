002ee0e08b15808361b9e0337faa1565c78fc05d
Matt-Chisholm <mathesonchisholm@hotmail.com>
2022-10-24 10:14:35 -0700

    initial clean up

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Chatbot from "react-chatbot-kit";
+
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <Chatbot />
+      </header>
+    </div>
+  );
+}
