862db15f70a58525a8e3f9e020ff78b1819c96bc
Matt-Chisholm <mathesonchisholm@hotmail.com>
2022-10-24 10:35:22 -0700

    bot button css

--- src/config.js
+++ src/config.js
@@ -1,7 +1,18 @@
 import { createChatBotMessage } from "react-chatbot-kit";
 
 const config = {
-  initialMessages: [createChatBotMessage(`Hello world`)],
+  botName: "Mattbot",
+  initialMessages: [
+    createChatBotMessage(`Hi, I'm here to help. What can I do for you?`),
+  ],
+  customStyles: {
+    botMessageBox: {
+      backgroundColor: "#376B7E",
+    },
+    chatButton: {
+      backgroundColor: "#376B7E",
+    },
+  },
 };
 
 export default config;
